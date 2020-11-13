bee4b116f081965bef55684d23103a16a93bd39b
Artin <lengthmin@gmail.com>
2020-11-13 15:19:20 +0800

    feat: return reponse with status_code

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -3,7 +3,7 @@ from flask import jsonify
 
 def resp_wrapper(code=200, msg="success", data=None):
     obj = {"code": code, "msg": msg, "data": data}
-    return jsonify(obj)
+    return jsonify(obj), code
 
 
 response = resp_wrapper
